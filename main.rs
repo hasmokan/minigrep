e1e720ebd062788b627779fc197800dcb3be11ac
hasmokan <1021056159@qq.com>
2025-01-14 14:08:11 +0800

    feat: extract logic

--- main.rs
+++ main.rs
@@ -1,7 +1,46 @@
 use std::env;
+use std::fs;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
+
+  let config = Config::new(&args);
+
   dbg!(&args);
+
+  println!("Searching for {}", config.query);
+  println!("In file {}", config.file_path);
+
+  let contents = fs::read_to_string(&config.file_path).expect("Should have been able to read the file");
+
+  println!("With text:\n{}", contents);
 }
 
+struct Config {
+  query: String,
+  file_path: String,
+}
+
+impl Config {
+  fn new(args: &[String]) -> Config {
+    if args.len() < 3 {
+      panic!("Not enough arguments");
+    }
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+  }
+
+  fn build(args: &[String]) -> Result<Config, &str>{
+    if args.len() < 3 {
+      return Err("Not enough arguments")
+    }
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Ok(Config { query, file_path })
+  }
+}
