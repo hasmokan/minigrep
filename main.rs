fefcf20a1e0a161f0147600c790888e74b75a920
hasmokan <1021056159@qq.com>
2025-01-15 22:12:10 +0800

    finish

--- main.rs
+++ main.rs
@@ -3,14 +3,12 @@ use std::env;
 use minigrep::Config;
 use minigrep::run;
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
-    dbg!(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
