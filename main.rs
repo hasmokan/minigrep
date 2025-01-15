0b8ed97c1dec843e96c0160aadbfdc2a1c0cf1bf
hasmokan <1021056159@qq.com>
2025-01-15 14:44:09 +0800

    feat: unit test

--- main.rs
+++ main.rs
@@ -1,46 +1,24 @@
 use std::env;
-use std::fs;
 
+use minigrep::Config;
+use minigrep::run;
 fn main() {
-  let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-  let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
-  dbg!(&args);
+    dbg!(&args);
 
-  println!("Searching for {}", config.query);
-  println!("In file {}", config.file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-  let contents = fs::read_to_string(&config.file_path).expect("Should have been able to read the file");
-
-  println!("With text:\n{}", contents);
-}
-
-struct Config {
-  query: String,
-  file_path: String,
-}
-
-impl Config {
-  fn new(args: &[String]) -> Config {
-    if args.len() < 3 {
-      panic!("Not enough arguments");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
     }
-
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-  }
-
-  fn build(args: &[String]) -> Result<Config, &str>{
-    if args.len() < 3 {
-      return Err("Not enough arguments")
-    }
-
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Ok(Config { query, file_path })
-  }
 }
+
+
